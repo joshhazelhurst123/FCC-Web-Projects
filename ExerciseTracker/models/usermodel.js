8257fcca889652e7335740a46f8eecc1f830374a
Josh <joshhazelhurst123@gmail.com>
2019-02-16 11:23:04 +1300

    Create usermodel.js

---
+++ ExerciseTracker/models/usermodel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: { type: String, unique: true, required: true }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
