c930046aa0102ef7eafcbffe98f7879cc249e0ff
Josh <joshhazelhurst123@gmail.com>
2019-02-16 11:41:38 +1300

    Update and rename usermodel.js to user.js

---
+++ ExerciseTracker/models/user.js
@@ -0,0 +1,15 @@
+
+const mongoose = require('mongoose');
+const shortid = require('shortid');
+
+const UserSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        default: shortid.generate
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    }
+})
